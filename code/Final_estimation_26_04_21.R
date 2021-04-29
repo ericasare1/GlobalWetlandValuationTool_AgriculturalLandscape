e20ee2dcce382a3f4e530b4ac771aabd400a3ba4
eric <eric.asare.kojo@gmail.com>
2021-04-29 07:18:11 -0600

    using the spTranform and writeOGR functions to create shp

--- code/Final_estimation_26_04_21.R
+++ code/Final_estimation_26_04_21.R
@@ -475,14 +475,34 @@ stargazer(lm_full_prov, lm_full_prov_ll, lm_full_reg, lm_full_reg_ll,
           single.row = TRUE)
 
 #.......Geo-spatial analysis----Create map of study locations
-pacman::p_load(sf, tmaptools, tmap)
+pacman::p_load(sf, tmaptools, tmap, raster, sp, rgdal)
 
 #Converting provision data to shp file
+#require(raster)
+
+
 df_prov_lat_log <- df_prov_acre %>%
   dplyr::select(latitude, longitude)
 
+points_crs <- CRS("+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0")
+coords <- df_prov_lat_log[c("longitude", "latitude")]
+
+prov_shp <- SpatialPointsDataFrame(
+  coords = coords,
+  df_prov_lat_log,
+  proj4string = points_crs)
+
+utm_prov <- spTransform(prov_shp, CRS("+init=epsg:32616")) #transform to UTM
+
+writeOGR(utm_prov, dsn="data/provision_shp/prov_shp", layer= "prov_shp", 
+         driver="ESRI Shapefile", overwrite_layer=TRUE)
+
+plot(utm_prov_shp)
 df_prov_shp <- st_as_sf(df_prov_lat_log, coords = c("latitude", "longitude"))
-st_crs(df_prov_shp)
+
+
+st_write(df_prov_shp,
+         "data/df_prov_shp.shp", driver = "ESRI Shapefile")
 
 #Converting regulation data to shp file
 df_reg_lat_log <- df_regul_acre %>%
